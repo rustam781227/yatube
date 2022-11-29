9adcb5418d7de2b0774e55a2277d265027357b9d
Rustam Salpagarov <photonzyx@gmail.com>
2022-11-29 16:59:41 +0300

    Admin, database, groups done

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,24 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Группа {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {group.title}'
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
